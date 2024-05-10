594886cc1f571ecbe323e6ccadc138fbbbc77e1c
Done-Brand <donedev8@gmail.com>
2024-05-10 09:18:09 +0200

    Created components
    At this point there won't be any content yet. Only the components and basic structure to our React project.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import "./style.css";
+
 function App() {
-  return;
+  const appTitle = "Today I Learned";
+
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            witdh="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
